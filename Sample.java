557b9c95236e144acaf91c1d1862f407bb66257f
Tring-Asbar <asbar.j@tringapps.com>
2025-01-28 15:53:39 +0530

    IntroMsg

---
+++ Sample.java
@@ -0,0 +1,7 @@
+public class Sample{
+    public static void main(String[] args){
+        System.out.print("Hello, Tringapps");
+        System.out.println("Myself Asbar");
+        System.out.println("Thanks for giving me  this wonderful Opportunity");
+    }
+}
